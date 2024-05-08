b6a3eda6902714b08d9c1e2b97b314aeff40daf6
sayantanigi <sayantan@goigi.in>
2024-05-08 15:32:13 +0530

    update code

--- application/views/portfolio.php
+++ application/views/portfolio.php
@@ -1,7 +1,16 @@
 <style>
-.modal-active{display:inline-block;top:64%}
-.Donate_Btn {top: 105% !important;}
-.ytp-title-link{display: none !important;}
+    .modal-active {
+        display: inline-block;
+        top: 64%
+    }
+
+    .Donate_Btn {
+        top: 105% !important;
+    }
+
+    .ytp-title-link {
+        display: none !important;
+    }
 </style>
 <section class="container Data_Container">
     <div class="row">
@@ -13,30 +22,51 @@
 <section class="main_section py-0 px-lg-5 ">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-8 col-md-12">
-                <div class="main_section_left">
-                    <div class="row">
-                        <?php
-                        if ($portfolio) {
-                        $i = 1;
-                        foreach ($portfolio as $value) { ?>
-                        <div class="col-md-5 mt-5 mb-5">
-                            <div class="prayer_images" style="height: 256px">
-                                <?php if(!file_exists("uploads/portfolio/image/".$value['file_link'])) { ?>
-                                <iframe src="<?= $value['file_link']?>" style="width: 100%; height: 256px; border-radius: 15px;"></iframe>
-                                <?php } else {?>
-                                <img src="<?php echo base_url()?>/uploads/portfolio/image/<?= $value['file_link']?>" style="height: 256px">
-                                <?php } ?>
-                                <div class="prayer_images_content">
-                                    <span><i class="fa fa-area-chart" aria-hidden="true"></i> <?php if(@$value['file_type'] == '1') { echo "Video";} else { echo "Image"; }?></span>
-                                    <p style="bottom: 35px; position: absolute;"><a href="<?= base_url() ?>portfolio_details/<?= $value['id'] ?>"><?= $value['title'] ?></a></p>
-                                </div>
+            <?php
+            if ($portfolio) {
+            $i = 1;
+            foreach ($portfolio as $value) {
+                $string = strip_tags($value['description']);
+                if (strlen($string) > 500) {
+                    $stringCut = substr($string, 0, 900);
+                    $endPoint = strrpos($stringCut, ' ');
+                    $string = $endPoint? substr($stringCut, 0, $endPoint) : substr($stringCut, 0);
+                    $string .= '    <a href="'.base_url().'portfolio_details/'.$value['id'].'" style="display: contents;">Learn More</a>';
+                }
+            ?>
+            <div class="col-md-12 mb-5">
+                <div class="row">
+                    <?php if ($i % 2 == 0) { ?>
+                    <div class="col-md-8 d-flex align-items-center text-justify" style="height: 256px;"><?= $string?></div>
+                    <?php } ?>
+                    <div class="col-md-4">
+                        <div class="prayer_images" style="height: 256px">
+                            <?php if (!file_exists("uploads/portfolio/image/" . $value['file_link'])) { ?>
+                            <iframe src="<?= $value['file_link'] ?>" style="width: 100%; height: 256px; border-radius: 15px;"></iframe>
+                            <?php } else { ?>
+                            <img src="<?php echo base_url() ?>/uploads/portfolio/image/<?= $value['file_link'] ?>" style="height: 256px">
+                            <?php } ?>
+                            <div class="prayer_images_content">
+                                <span>
+                                <i class="fa fa-area-chart" aria-hidden="true"></i>
+                                <?php if (@$value['file_type'] == '1') {
+                                    echo "Video";
+                                } else {
+                                    echo "Image";
+                                } ?>
+                                </span>
+                                <p style="bottom: 35px; position: absolute;">
+                                    <a href="<?= base_url() ?>portfolio_details/<?= $value['id'] ?>"><?= $value['title'] ?></a>
+                                </p>
                             </div>
                         </div>
-                        <?php $i++; } } ?>
                     </div>
+                    <?php if ($i % 2 != 0) { ?>
+                    <div class="col-md-8 d-flex align-items-center text-justify" style="height: 256px;"><?= $string?></div>
+                    <?php } ?>
                 </div>
             </div>
+            <?php $i++; } } ?>
         </div>
     </div>
 </section>
@@ -55,7 +85,7 @@
 </section>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 <script>
-$(window).on('load', function() {
-    $('.ytp-title-link').remove();
-});
+    $(window).on('load', function () {
+        $('.ytp-title-link').remove();
+    });
 </script>
